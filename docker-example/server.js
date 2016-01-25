263e1f2d3f9c9724c274954f654e36e755e53452
Jared Ladner <jared.ladner@geocent.com>
2016-01-25 13:43:08 -0600

    changing from plain text to html resposne

--- docker-example/server.js
+++ docker-example/server.js
@@ -5,10 +5,10 @@ http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {'Content-Type': 'text/html'});
    
    // Send the response body as “Automation for the People”
-   response.end('Automation for the People');
+   response.end('<html><h3>Automation for the People</h3></html>');
 }).listen(8888);
 
 // Console will print the message
